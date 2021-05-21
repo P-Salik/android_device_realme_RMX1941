078b627c037f323cfab8e4602da18a8539346063
SamarV-121 <samarvispute121@gmail.com>
2021-05-21 21:46:05 +0530

    RMX1941: Build init.recovery.mt6765.rc
    Change-Id: I42a442b572a86d1e0fb5b7b05599b959ebd793d1
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -288,6 +288,10 @@ PRODUCT_PACKAGES += \
     RealmeDirac \
     RealmeParts
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.mt6765.rc
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
