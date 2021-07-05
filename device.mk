fd4548be41349026f91e225e3e3c9689ce7e269b
Frasczhe97 <anprasetya97@gmail.com>
2021-07-05 10:57:28 +0530

    RMX1941: Drop InProcessNetworkStack and com.android.tethering.inprocess
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -144,10 +144,6 @@ PRODUCT_PACKAGES += \
     WifiOverlay \
     wpa_supplicant.conf
 
-# Do not spin up a separate process for the network stack, use an in-process APK.
-PRODUCT_PACKAGES += InProcessNetworkStack
-PRODUCT_PACKAGES += com.android.tethering.inprocess
-
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
