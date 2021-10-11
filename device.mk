abc129f9a608086759ba1cc84eb626423a9c981e
TheMalachite <eliasgheeraert@gmail.com>
2021-10-11 11:06:06 +0530

    RMX1941: Build libldacBT_dec, libldacBT_bco and libbtconfigstore from source
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbtconfigstore \
+    libldacBT_bco \
+    libldacBT_dec
 
 # Dependencies of kpoc_charger
 PRODUCT_PACKAGES += \
