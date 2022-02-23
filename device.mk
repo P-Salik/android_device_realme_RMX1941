70e97c3506fc48c6ae72dea8d59d32792fe4f3f4
kubersharma001 <kubersharma001@gmail.com>
2022-02-23 16:29:53 +0000

    RMX1941: Remove non-existent libldacBT_{bco,dec} build rules - These seem to be long gone/not available to be source compiled? - Anyways, these seem to have no actual function and are not needed other than to avoid logspams
    unknown target 'libldacBT_dec'
    unknown target 'libldacBT_bco'
    
    Change-Id: Ia65d4e72128352132a7ad1a00c61d9596ece714a

--- device.mk
+++ device.mk
@@ -75,9 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0.vendor \
-    libbtconfigstore \
-    libldacBT_bco \
-    libldacBT_dec
+    libbtconfigstore 
 
 # Camera
 PRODUCT_PACKAGES += \
