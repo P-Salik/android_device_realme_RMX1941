10bbaee807840249413c539275e327796d7255e8
Tim Zimmermann <tim@linux4.de>
2022-06-13 09:19:40 +0530

    RMX1941: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,8 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1941)
 
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+  include $(call all-makefiles-under,$(LOCAL_PATH))
 
   GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
   GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
