effb64c06ae72bac681afa5e53d8454d30183257
P-Salik <peersalik16@gmail.com>
2022-06-13 23:40:19 +0530

    RMX1941: Move device manifests to configs
    * Also specify light aidl entry as vintf fragment for lights hal in a separate manifest
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,8 +59,8 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # HIDL
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
