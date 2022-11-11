f511c8bbf04ef1f9ef410dc747b2bff99316e991
SamarV-121 <samarvispute121@pm.me>
2022-11-11 16:56:13 +0530

    RMX1941: Patch libMtkOmxVdecEx to use libui-v32
     * Copy libui-v32 from vndk prebuilts to avoid conflicts with hardware/lineage/compat
     * Fixes OMX crash https://gist.githubusercontent.com/SamarV-121/efbeed785308c09655c354362b0f007c/raw
    
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Id7f221f109e27a4d5544aa81ec73c60db0c0dc33

--- device.mk
+++ device.mk
@@ -327,7 +327,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/android.hardware.graphics.common-V2-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.graphics.common-V2-ndk_platform.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libmedia_helper.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmedia_helper-v29.so \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v32.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcompiler_rt.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcompiler_rt.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v29.so \
     prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v30.so \
